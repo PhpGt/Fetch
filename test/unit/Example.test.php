e9e2a7a09136c9b47efb1e00df0acb12d2812de1
Greg Bowler <greg.bowler@g105b.com>
2016-09-29 22:41:09 +0100

    Require autoload in helper function.

--- test/unit/Example.test.php
+++ test/unit/Example.test.php
@@ -1,9 +1,14 @@
 <?php
 namespace phpgt\fetch;
 
+require_once __DIR__ . "/../../vendor/autoload.php";
+
 class ExampleTest extends \PHPUnit_Framework_TestCase {
 
 public function testTest() {
+	$http = new Http();
+	$http->request("test");
+
 	$this->assertTrue(true);
 }
 
