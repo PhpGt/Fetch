18fc2b92664923970997bb71231903a52ec139e7
Greg Bowler <greg.bowler@g105b.com>
2016-09-30 22:48:03 +0100

    Mock promise resolution.

--- test/unit/Example.test.php
+++ test/unit/Example.test.php
@@ -5,11 +5,43 @@ require_once __DIR__ . "/../../vendor/autoload.php";
 
 class ExampleTest extends \PHPUnit_Framework_TestCase {
 
-public function testTest() {
-	$http = new Http();
-	$http->request("test");
+const URL = "https://github.com/phpgt/fetch";
 
-	$this->assertTrue(true);
+public function testPromiseReturned() {
+	$http = new Http();
+	$promise = $http->request(self::URL);
+	$this->assertInstanceOf("\GuzzleHttp\Promise\Promise", $promise);
+
+	$promise2 = $http->all();
+	$this->assertInstanceOf("\GuzzleHttp\Promise\Promise", $promise2);
 }
+
+/**
+ * @group failing
+ */
+public function testCallbackInvoked() {
+	$mockPromise = $this->getMock("\GuzzleHttp\Promise\Promise");
+
+	$mockCallback = $this->getMock("stdClass", ["mockCallback"]);
+	$mockCallback->expects($this->once())
+		->method("mockCallback")
+		->will($this->returnValue($mockPromise));
+
+	$http = new Http();
+	$http->request(self::URL)
+		->then([$mockCallback, "mockCallback"]);
+
+	$http->wait();
+}
+
+public function testResponseType() {
+	$http = new Http();
+	$http->request(self::URL)
+		->then(function($response) {
+			$this->assertInstanceOf("\phpgt\fetch\Response", $response);
+		});
+	$http->wait();
+}
+
 
 }#
