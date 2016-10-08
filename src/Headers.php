6a8b4c5b1b725ffc746d8aeb32d1d9456917d44d
Greg Bowler <greg.bowler@g105b.com>
2016-10-08 13:49:46 +0100

    Outline Headers class. For #9.

--- src/Headers.php
+++ src/Headers.php
@@ -2,15 +2,70 @@
 namespace phpgt\fetch;
 
 /**
- * Allows you to perform various actions on HTTP request and response headers.
- * These actions include retrieving, setting, adding to, and removing.
- * A Headers object has an associated header list, which is initially empty
- * and consists of zero or more name and value pairs. You can add to this
- * using methods like append() (see Examples.) In all methods of this
- * interface, header names are matched by case-insensitive byte sequence.
-
-
+ * The Headers interface of the Fetch API allows you to perform various actions
+ * on HTTP request and response headers. These actions include retrieving,
+ * setting, adding to, and removing. A Headers object has an associated header
+ * list, which is initially empty and consists of zero or more name and
+ * value pairs.
  */
 class Headers {
+
+public function __construct(array $init = []) {
+
+}
+
+/**
+ * Appends a new value onto an existing header inside a Headers object, or
+ * adds the header if it does not already exist.
+ */
+public function append(string $name, string $value) {
+
+}
+
+/**
+ * Deletes a header from the current Headers object. Throws a HeaderException
+ * if the provided name does not exist.
+ */
+public function delete(string $name) {
+
+}
+
+/**
+ * Returns the first value of a given header from within a Headers object. If
+ * the requested header doesn't exist in the Headers object, the call
+ * returns null.
+ */
+public function get(string $name) {
+
+}
+
+/**
+ * Returns an array of all the values of a header within a Headers object with
+ * a given name. If the requested header doesn't exist in the Headers object,
+ * it returns an empty array.
+ */
+public function getAll():array {
+
+}
+
+/**
+ * Returns a boolean stating whether a Headers object contains a certain header.
+ */
+public function has(string $name):bool {
+
+}
+
+/**
+ * Sets a new value for an existing header inside a Headers object, or adds the
+ * header if it does not already exist.
+ *
+ * The difference between set() and append() is that if the specified header
+ * already exists and accepts multiple values, set() overwrites the existing
+ * value with the new one, whereas append() appends the new value to the end of
+ * the set of values.
+ */
+public function set(string $name, string $value) {
+
+}
 
 }#
