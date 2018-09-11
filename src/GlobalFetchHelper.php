7a1772300abda1cb08348ba1fb9a7c89f075bd12
Greg Bowler <greg.bowler@g105b.com>
2018-09-11 22:14:48 +0100

    Fix tests

--- src/GlobalFetchHelper.php
+++ src/GlobalFetchHelper.php
@@ -16,37 +16,37 @@ use Http\Promise\Promise;
  */
 abstract class GlobalFetchHelper implements GlobalFetch {
 
-const HTTP_METHODS = [
-	"GET",
-	"HEAD",
-	"POST",
-	"PUT",
-	"DELETE",
-	"CONNECT",
-	"OPTIONS",
-	"TRACE",
-	"PATCH",
-];
+	const HTTP_METHODS = [
+		"GET",
+		"HEAD",
+		"POST",
+		"PUT",
+		"DELETE",
+		"CONNECT",
+		"OPTIONS",
+		"TRACE",
+		"PATCH",
+	];
 
-public function __call($name, $args) {
-	$method = strtoupper($name);
-	if(!in_array($method, self::HTTP_METHODS)) {
-		trigger_error(
-			"Call to undefined method "
-			. __CLASS__
-			. "::"
-			. $name
-			. "()"
-			,
-			E_USER_ERROR
-		);
+	public function __call($name, $args) {
+		$method = strtoupper($name);
+		if(!in_array($method, self::HTTP_METHODS)) {
+			trigger_error(
+				"Call to undefined method "
+				. __CLASS__
+				. "::"
+				. $name
+				. "()"
+				,
+				E_USER_ERROR
+			);
+		}
+
+		$init = $args[1] ?? [];
+		$init["method"] = $method;
+		$args[1] = $init;
+
+		return call_user_func_array([$this, "fetch"], $args);
 	}
 
-	$init = $args[1] ?? [];
-	$init["method"] = $method;
-	$args[1] = $init;
-
-	return call_user_func_array([$this, "fetch"], $args);
 }
-
-}#
--- test/unit/GlobalFetchHelper.test.php
+++ test/unit/GlobalFetchHelper.test.php
@@ -1,56 +1,37 @@
 <?php
 namespace Gt\Fetch;
 
-class GlobalFetchHelperTest extends \PHPUnit_Framework_TestCase {
+use PHPUnit\Framework\Error\Error;
+use PHPUnit\Framework\TestCase;
 
-/**
- * Asserts that the fetch method is called for each of the magic methods on the
- * GlobalFetchHelper abstract class
- */
-public function testHttpMethods() {
-	$observerArray = [];
+class GlobalFetchHelperTest extends TestCase {
 
-	foreach(GlobalFetchHelper::HTTP_METHODS as $httpMethod) {
-		$lowerCaseMethod = strtolower($httpMethod);
-		$uri = "fake://example.com/$lowerCaseMethod-test";
+	/**
+	 * Asserts that the fetch method is called for each of the magic methods on the
+	 * GlobalFetchHelper abstract class
+	 */
+	public function testHttpMethods() {
+		$observerArray = [];
 
-		$initArray = [
-			"method" => $httpMethod,
-		];
+		foreach(GlobalFetchHelper::HTTP_METHODS as $httpMethod) {
+			$lowerCaseMethod = strtolower($httpMethod);
+			$uri = "fake://example.com/$lowerCaseMethod-test";
 
-		$observerArray[$httpMethod] = $this->getMockBuilder(
-			GlobalFetchHelper::class
-		)
-	    	->setMethods(["fetch"])
-	    	->getMock();
+			$initArray = [
+				"method" => $httpMethod,
+			];
 
-		$observerArray[$httpMethod]->expects($this->once())
-		    ->method("fetch")
-		    ->with($uri, $initArray);
+			$observerArray[$httpMethod] = $this->getMockBuilder(
+				GlobalFetchHelper::class
+			)
+				->setMethods(["fetch"])
+				->getMock();
 
-		$observerArray[$httpMethod]->$lowerCaseMethod($uri);
+			$observerArray[$httpMethod]->expects($this->once())
+				->method("fetch")
+				->with($uri, $initArray);
+
+			$observerArray[$httpMethod]->$lowerCaseMethod($uri);
+		}
 	}
 }
-
-/**
- * Calling a magic method that doesn't match an allowed HTTP method should
- * behave exactly like calling a non-existent method should, by emitting an
- * error.
- *
- * @expectedException PHPUnit_Framework_Error
- */
-public function testNonHttpMethod() {
-	$httpMethod = "UNKNOWN";
-	$lowerCaseMethod = strtolower($httpMethod);
-	$uri = "fake://example.com/$lowerCaseMethod-test";
-
-	$observer = $this->getMockBuilder(
-		GlobalFetchHelper::class
-	)
-	->setMethods(["fetch"])
-	->getMock();
-
-	$observer->$lowerCaseMethod($uri);
-}
-
-}#
