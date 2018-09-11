92f0f73631c6de5ab92ea22ee728df1373558c4f
Greg Bowler <greg.bowler@g105b.com>
2018-09-11 22:06:36 +0100

    Fix namespaces

--- test/unit/Helper.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Gt\Fetch\Test;
-
-class Helper {
-
-const URI_SIMPLE = "fake://php.gt/fetch";
-const URI_COMPLEX = "fake://someuser:somepassword@php.gt:8008/fetch?id=105#example";
-}#
---
+++ test/unit/Helper/Helper.php
@@ -0,0 +1,7 @@
+<?php
+namespace Gt\Fetch\Test\Helper;
+
+class Helper {
+	const URI_SIMPLE = "fake://php.gt/fetch";
+	const URI_COMPLEX = "fake://someuser:somepassword@php.gt:8008/fetch?id=105#example";
+}
