e5f53c53644c5cfb18fca73b99b18e1cf686bff4
Greg Bowler <greg.bowler@g105b.com>
2018-11-01 23:43:10 +0000

    Introduce async write function

--- src/RequestResolver.php
+++ src/RequestResolver.php
@@ -12,25 +12,28 @@ use React\EventLoop\LoopInterface;
 use React\Promise\Deferred;
 
 class RequestResolver {
+	/** @var LoopInterface */
 	protected $loop;
-
-	/** @var CurlInterface[] */
-	protected $curlReferenceList;
-	/** @var Deferred[] */
-	protected $deferredReferenceList;
-	/** @var Response[] */
-	protected $responseReferenceList;
-
 	/** @var CurlMultiInterface */
 	protected $curlMulti;
+
+	/** @var CurlInterface[] */
+	protected $curlList;
+	/** @var Deferred[] */
+	protected $deferredList;
+	/** @var Response[] */
+	protected $responseList;
+	/** @var string[] */
+	protected $headersList;
 
 	public function __construct(
 		LoopInterface $loop,
 		string $curlMultiClass = CurlMulti::class
 	) {
 		$this->loop = $loop;
-		$this->curlReferenceList = [];
-		$this->deferredReferenceList = [];
+		$this->curlList = [];
+		$this->deferredList = [];
+		$this->headersList = [];
 		$this->curlMulti = new $curlMultiClass();
 	}
 
@@ -45,75 +48,79 @@ class RequestResolver {
 			$curl->setOptArray($init["curlopt"]);
 		}
 
-		$curl->setOpt(CURLOPT_RETURNTRANSFER, true);
+		$curl->setOpt(CURLOPT_RETURNTRANSFER, false);
 		$curl->setOpt(CURLOPT_HEADER, true);
+		$curl->setOpt(CURLOPT_WRITEFUNCTION, [$this, "write"]);
 
 		$this->curlMulti->add($curl);
-		$this->curlReferenceList []= $curl;
-		$this->deferredReferenceList []= $deferred;
-		$this->responseReferenceList []= new Response();
+		$this->curlList []= $curl;
+		$this->deferredList []= $deferred;
+		$this->headersList []= "";
+		$this->responseList []= new Response();
+	}
+
+	public function write($chIncoming, $content):int {
+		$match = false;
+		foreach($this->curlList as $i => $curl) {
+			if($chIncoming === $curl->getHandle()) {
+				$match = true;
+				break;
+			}
+		}
+
+		if(!$match) {
+			// TODO: Throw exception.
+			die("NO CURL HANDLE!!!!");
+		}
+
+		if(!strstr($this->headersList[$i], "\r\n\r\n")) {
+			echo "1";
+			if(strstr($content, "\r\n\r\n")) {
+				$parser = new Parser($this->headersList[$i]);
+				$this->responseList[$i] = $this->responseList[$i]->withProtocolVersion(
+					$parser->getProtocolVersion()
+				);
+				$this->responseList[$i] = $this->responseList[$i]->withStatus(
+					$parser->getStatusCode()
+				);
+				foreach($parser->getKeyValues() as $key => $value) {
+					$this->responseList[$i] = $this->responseList[$i]->withAddedHeader(
+						$key,
+						$value
+					);
+				}
+			}
+
+			$this->headersList[$i] .= $content;
+		}
+		else {
+			echo "2";
+			$body = $this->responseList[$i]->getBody();
+			$body->write($content);
+			$this->deferredList[$i]->resolve($body);
+		}
+
+		echo ".";
+
+		return strlen($content);
 	}
 
 	public function tick():void {
 		$active = 0;
 
-		ob_start();
-		$curlMultiCode = $this->curlMulti->exec($active);
-		if($curlMultiCode !== CURLM_OK) {
+		do {
+			$status = $this->curlMulti->exec($active);
+		}
+		while($status === CURLM_CALL_MULTI_PERFORM);
+
+		if($status !== CURLM_OK) {
 			// TODO: Throw exception.
 			die("ERROR!");
 		}
-		ob_end_clean();
 
-		if($state = $this->curlMulti->infoRead()) {
-			$ch = $state->getHandle();
-
-			foreach($this->curlReferenceList as $i => $curlReference) {
-				if($ch !== $curlReference) {
-					continue;
-				}
-
-				$content = $this->curlMulti->getContent(
-					$ch
-				);
-				$response = $this->responseReferenceList[$i];
-
-				list(
-					$headerString,
-					$bodyString
-				) = explode(
-					"\r\n\r\n",
-					$content
-				);
-
-
-				$headerParser = new Parser($headerString);
-				$response = $response->withProtocolVersion(
-					$headerParser->getProtocolVersion()
-				);
-				$response = $response->withStatus(
-					$headerParser->getStatusCode()
-				);
-
-				foreach($headerParser->getKeyValues() as $key => $value) {
-					$response = $response->withAddedHeader(
-						$key,
-						$value
-					);
-				}
-
-// Note: Updating the original reference, as $response is immutable.
-				$this->responseReferenceList[$i] = $response;
-				$body = $response->getBody();
-
-				if($response->getStatusCode()) {
-					$body->write($bodyString);
-				}
-
-				$this->deferredReferenceList[$i]->resolve(
-					BodyResponseFactory::fromResponse($response)
-				);
-			}
+		if($active === 0) {
+			$this->loop->stop();
+			return;
 		}
 	}
 }
