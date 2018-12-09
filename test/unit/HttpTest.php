b2ae162a071b084a30851cef13e91ef3bd2e8272
Greg Bowler <greg.bowler@g105b.com>
2018-12-09 20:42:05 +0000

    Test sendAsyncRequest

--- test/unit/HttpTest.php
+++ test/unit/HttpTest.php
@@ -10,11 +10,13 @@ use Gt\Fetch\Test\Helper\TestCurl;
 use Gt\Fetch\Test\Helper\TestCurlMulti;
 use Gt\Http\Request;
 use Gt\Http\Uri;
+use Http\Promise\Promise as HttpPromise;
 use PHPUnit\Framework\TestCase;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 
 class HttpTest extends TestCase {
+	/** @runInSeparateProcess */
 	public function testFetchBodyResponsePromise() {
 		$http = new Http(
 			[],
@@ -34,6 +36,7 @@ class HttpTest extends TestCase {
 		self::assertEquals(999, $fakeStatus);
 	}
 
+	/** @runInSeparateProcess */
 	public function testSendRequest() {
 		$htmlHelloFetch = "<!doctype html><h1>Hello, Fetch!</h1>";
 		ResponseSimulator::setExpectedBody($htmlHelloFetch);
@@ -64,5 +67,42 @@ class HttpTest extends TestCase {
 		);
 	}
 
+	/** @runInSeparateProcess */
+	public function testAsyncRequest() {
+		$htmlHelloFetch = "<!doctype html><h1>Hello, Fetch!</h1>";
+		ResponseSimulator::setExpectedBody($htmlHelloFetch);
 
+		$http = new Http(
+			[],
+			0.01,
+			TestCurl::class,
+			TestCurlMulti::class
+		);
+
+		$uri = self::createMock(Uri::class);
+		$request = self::createMock(Request::class);
+		$request->method("getUri")
+			->willReturn($uri);
+
+		/** @var RequestInterface $request */
+
+		$promise = $http->sendAsyncRequest($request);
+		self::assertInstanceOf(
+			Promise::class,
+			$promise
+		);
+
+		$fakeStatus = null;
+
+		$promise->then(function(BodyResponse $response) use(&$fakeStatus) {
+			$fakeStatus = $response->status;
+		});
+
+		$http->wait();
+
+		self::assertEquals(
+			999,
+			$fakeStatus
+		);
+	}
 }
