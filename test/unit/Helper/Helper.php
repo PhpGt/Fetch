bc10e31b2a25f232289b72e4ad6d3a7865af30ab
Greg Bowler <greg.bowler@g105b.com>
2018-12-09 18:18:59 +0000

    Remove old tests

--- test/unit/Helper/Helper.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace Gt\Fetch\Test\Helper;
-
-class Helper {
-	const URI_SIMPLE = "fake://php.gt/fetch";
-	const URI_COMPLEX = "fake://someuser:somepassword@php.gt:8008/fetch?id=105#example";
-}
