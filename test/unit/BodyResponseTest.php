037b800b9a66eb45d9e26a057c58081852c806c3
Greg Bowler <greg.bowler@g105b.com>
2019-05-01 10:56:29 +0100

    Test JSON resolution

--- test/unit/BodyResponseTest.php
+++ test/unit/BodyResponseTest.php
@@ -7,6 +7,7 @@ use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use Psr\Http\Message\StreamInterface;
 use React\EventLoop\LoopInterface;
+use stdClass;
 
 class BodyResponseTest extends TestCase {
 	public function testText() {
@@ -32,5 +33,32 @@ class BodyResponseTest extends TestCase {
 		$sut->endDeferredResponse();
 
 		self::assertEquals($exampleContents, $sutOutput);
+	}
+
+	public function testJson() {
+		$exampleObj = new StdClass();
+		$exampleObj->test = "Example";
+		$jsonString = json_encode($exampleObj);
+
+		/** @var MockObject|LoopInterface $loop */
+		$loop = self::createMock(LoopInterface::class);
+		/** @var MockObject|StreamInterface $stream */
+		$stream = self::createMock(StreamInterface::class);
+		$stream->method("tell")
+			->willReturn(0);
+		$stream->method("getContents")
+			->willReturn($jsonString);
+
+		$sutOutput = null;
+		$sut = new BodyResponse();
+		$sut = $sut->withBody($stream);
+		$sut->startDeferredResponse($loop);
+		$promise = $sut->json();
+		$promise->then(function($fulfilledValue)use(&$sutOutput) {
+			$sutOutput = $fulfilledValue;
+		});
+		$sut->endDeferredResponse();
+
+		self::assertEquals($exampleObj, $sutOutput);
 	}
 }
