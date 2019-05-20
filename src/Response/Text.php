5133ca477f1a9ee88bd672451d29a6fbc60834c3
Greg Bowler <greg.bowler@g105b.com>
2019-05-20 15:03:08 +0100

    Complete text response type with comment

--- src/Response/Text.php
+++ src/Response/Text.php
@@ -1,6 +1,12 @@
 <?php
 namespace Gt\Fetch\Response;
 
+/**
+ * This class represents a string response. The reason it wraps a string with an
+ * object is to allow for the same code to construct any type of response, and
+ * in turn means that the Gt\Fetch\Response namespace is a good reference for
+ * the types of response possible.
+ */
 class Text {
 	/** @var string */
 	protected $content;
