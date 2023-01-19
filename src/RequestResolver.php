77aa1537ad4b51d45827f4f02bb91d91596f95aa
Greg Bowler <greg.bowler@g105b.com>
2023-01-19 22:46:50 +0000

    wip: tidy request resolver

--- src/RequestResolver.php
+++ src/RequestResolver.php
@@ -10,29 +10,26 @@ use Gt\Promise\Deferred;
 use Psr\Http\Message\UriInterface;
 
 class RequestResolver {
-	protected Loop $loop;
-
 	/** @var array<CurlMultiInterface> */
-	protected array $curlMultiList;
+	private array $curlMultiList;
 	/** @var array<CurlInterface> */
-	protected array $curlList;
+	private array $curlList;
 	/** @var array<Deferred> */
-	protected array $deferredList;
+	private array $deferredList;
 	/** @var array<BodyResponse> */
-	protected array $responseList;
+	private array $responseList;
 	/** @var array<string> */
-	protected array $headerList;
+	private array $headerList;
 	/** @var array<string> */
-	protected array $integrityList;
+	private array $integrityList;
 	/** @var array<object> */
-	protected array $signalList;
+	private array $signalList;
 
 	public function __construct(
-		Loop $loop,
+		private readonly Loop $loop,
 		private readonly string $curlClass,
 		private readonly string $curlMultiClass,
 	) {
-		$this->loop = $loop;
 		$this->curlMultiList = [];
 		$this->curlList = [];
 		$this->deferredList = [];
@@ -42,12 +39,22 @@ class RequestResolver {
 		$this->signalList = [];
 	}
 
+	/**
+	 * Adds a new job to resolve. An HTTP request will be made to the
+	 * provided UriInterface via curl_multi, using the provided options
+	 * array. The Deferred object will be used to pass back the resolved
+	 * BodyResponse, or rejected Throwable.
+	 * The optional $integrity value will match the response's body with the
+	 * provided hash as Subresource Identity (SRI).
+	 * The optional Controller $signal is used internally by curl to provide
+	 * an abort signal.
+	 */
 	public function add(
 		UriInterface $uri,
 		array $curlOptArray,
 		Deferred $deferred,
 		string $integrity = null,
-		Controller $signal = null
+		Controller $signal = null,
 	):void {
 		/** @var CurlInterface $curl */
 		$curl = new $this->curlClass($uri);
@@ -74,80 +81,16 @@ class RequestResolver {
 
 		$this->loop->addDeferredToTimer($deferred);
 
+		$bodyResponse = new BodyResponse();
+		$bodyResponse->startDeferredResponse($this->loop, $curl);
+
 		array_push($this->curlList, $curl);
 		array_push($this->curlMultiList, $curlMulti);
 		array_push($this->deferredList, $deferred);
 		array_push($this->integrityList, $integrity);
-		$bodyResponse = new BodyResponse();
-		$bodyResponse->startDeferredResponse($this->loop, $curl);
 		array_push($this->responseList, $bodyResponse);
 		array_push($this->headerList, "");
 		array_push($this->signalList, $signal);
-	}
-
-	public function writeHeader($ch, string $rawHeader):int {
-		$i = $this->getIndex($ch);
-		$headerLine = trim($rawHeader);
-
-// If $headerLine is empty, it represents the last line before the body starts.
-// HTTP headers always end on an empty line. See https://www.w3.org/Protocols/rfc2616/rfc2616-sec4.html
-		if($headerLine === "") {
-			$parser = new Parser($this->headerList[$i]);
-			$this->responseList[$i] = $this->responseList[$i]->withProtocolVersion(
-				$parser->getProtocolVersion()
-			);
-			$this->responseList[$i] = $this->responseList[$i]->withStatus(
-				$parser->getStatusCode()
-			);
-
-			foreach($parser->getKeyValues() as $key => $value) {
-				if(empty($key)) {
-					continue;
-				}
-
-				$this->responseList[$i] = $this->responseList[$i]->withAddedHeader(
-					$key,
-					$value
-				);
-			}
-		}
-
-		$this->headerList[$i] .= $rawHeader;
-
-// To indicate that this function has successfully run, cURL expects it to
-// return the number of bytes read. If this does not match the same number
-// that cURL sees, cURL will drop the connection.
-		return strlen($rawHeader);
-	}
-
-	public function writeBody($ch, $content):int {
-		$i = $this->getIndex($ch);
-
-		$body = $this->responseList[$i]->getBody();
-		$body->write($content);
-
-		if($this->deferredList[$i]) {
-			$this->deferredList[$i]->resolve($this->responseList[$i]);
-			$this->deferredList[$i] = null;
-		}
-
-// To indicate that this function has successfully run, cURL expects it to
-// return the number of bytes read. If this does not match the same number
-// that cURL sees, cURL will drop the connection.
-		return strlen($content);
-	}
-
-	/** @noinspection PhpUnusedParameterInspection */
-	public function progress(
-		$ch,
-		int $expectedDownloadedBytes,
-		int $downloadedBytes,
-		int $expectedUploadedBytes,
-		int $uploadedBytes
-	):int {
-		$index = $this->getIndex($ch);
-		return (int)$this->signalList[$index]?->aborted;
-
 	}
 
 	public function tick():void {
@@ -186,7 +129,71 @@ class RequestResolver {
 		}
 	}
 
-	protected function getIndex($chIncoming):int {
+	private function writeHeader($ch, string $rawHeader):int {
+		$i = $this->getIndex($ch);
+		$headerLine = trim($rawHeader);
+
+// If $headerLine is empty, it represents the last line before the body starts.
+// HTTP headers always end on an empty line. See https://www.w3.org/Protocols/rfc2616/rfc2616-sec4.html
+		if($headerLine === "") {
+			$parser = new Parser($this->headerList[$i]);
+			$this->responseList[$i] = $this->responseList[$i]->withProtocolVersion(
+				$parser->getProtocolVersion()
+			);
+			$this->responseList[$i] = $this->responseList[$i]->withStatus(
+				$parser->getStatusCode()
+			);
+
+			foreach($parser->getKeyValues() as $key => $value) {
+				if(empty($key)) {
+					continue;
+				}
+
+				$this->responseList[$i] = $this->responseList[$i]->withAddedHeader(
+					$key,
+					$value
+				);
+			}
+		}
+
+		$this->headerList[$i] .= $rawHeader;
+
+// To indicate that this function has successfully run, cURL expects it to
+// return the number of bytes read. If this does not match the same number
+// that cURL sees, cURL will drop the connection.
+		return strlen($rawHeader);
+	}
+
+	private function writeBody($ch, string $content):int {
+		$i = $this->getIndex($ch);
+
+		$body = $this->responseList[$i]->getBody();
+		$body->write($content);
+
+		if($this->deferredList[$i]) {
+			$this->deferredList[$i]->resolve($this->responseList[$i]);
+			$this->deferredList[$i] = null;
+		}
+
+// To indicate that this function has successfully run, cURL expects it to
+// return the number of bytes read. If this does not match the same number
+// that cURL sees, cURL will drop the connection.
+		return strlen($content);
+	}
+
+	/** @noinspection PhpUnusedParameterInspection */
+	private function progress(
+		$ch,
+		int $expectedDownloadedBytes,
+		int $downloadedBytes,
+		int $expectedUploadedBytes,
+		int $uploadedBytes
+	):int {
+		$index = $this->getIndex($ch);
+		return (int)$this->signalList[$index]?->aborted;
+	}
+
+	private function getIndex($chIncoming):int {
 		$i = -1;
 		$match = false;
 		foreach($this->curlList as $i => $curl) {
